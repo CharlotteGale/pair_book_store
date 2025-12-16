6d0a4c9b10c25d802786f3c175f130da8ed73852
Mitchell <mitch.lewsey@gmail.com>
2025-12-16 14:34:44 +0000

    started bookstore all tests

---
+++ tests/test_book_store_repository.py
@@ -0,0 +1,14 @@
+from lib.book_store_repository import *
+from lib.book_store import *
+
+"""
+Given calling book_store_repository#all
+Returns list of Book objects
+"""
+
+def test_book_store_repository_all_returns_list_book_objects(db_connection):
+    db_connection.seed("seeds/book_store.sql")
+    repository = BookStoreRepository(db_connection)
+    books = repository.all()
+    assert books[0] == Book('1, Nineteen Eighty-Four, George Orwell')
+
